f1ba01a1c41c45ef04b2a0a3e72d72f6fa4809ed
Ankitha Honnaiah <ankitha.honnaiah@magna.com>
2025-08-26 14:46:01 +0530

    Added calc_discount - v1.1.0

--- corelib/_init_.py
+++ corelib/_init_.py
@@ -1,2 +1,4 @@
 from .math_utils import add, subtract
 from .string_utils import shout
+from .math_utils import add, subtract, calc_discount
+from .string_utils import shout
--- corelib/math_utils.py
+++ corelib/math_utils.py
@@ -3,3 +3,7 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+def calc_discount(price, percentage):
+    """Calculate discounted price"""
+    return price - (price * percentage / 100.0)
