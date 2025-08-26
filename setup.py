94796837c7867d3b0baa32dfbe47d34682c71255
Ankitha Honnaiah <ankitha.honnaiah@magna.com>
2025-08-26 14:59:58 +0530

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="corelib",
+    version="1.0.0",  # will get updated with each release
+    packages=find_packages(),
+    install_requires=[],
+)
