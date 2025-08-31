0d9301838413f337bff574b0dab45594a9a935f5
Ankitha Honnaiah <ankitha.honnaiah@magna.com>
2025-09-01 00:21:26 +0530

    corelib v2.1.0: replace calc_discount with apply_discount

--- corelib/math_utils.py
+++ corelib/math_utils.py
@@ -4,6 +4,6 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
-def calc_discount(price, percentage):
-    """Calculate discounted price"""
-    return price - (price * percentage / 100.0)
+def apply_discount(price, percentage):
+    """Apply discount in a precise way"""
+    return round(price - (price * percentage / 100.0), 2)
