10e5738d3cfe679d7f3c81edaddc1a0ebbf813f6
Ankitha Honnaiah <ankitha.honnaiah@magna.com>
2025-09-01 00:53:17 +0530

    corelib: fixing version v1.0.0

--- corelib/math_utils.py
+++ corelib/math_utils.py
@@ -1,9 +1,3 @@
-# math_utils.py
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-def apply_discount(price, percentage):
-    """Apply discount in a precise way"""
-    return round(price - (price * percentage / 100.0), 2)
+# string_utils.py
+def shout(text):
+    return text.upper() + "!"
